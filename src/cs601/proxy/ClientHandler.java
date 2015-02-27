f87e2da9ed8095cd2a27a83315aa4e38f04a0662
Terence Parr <parrt@antlr.org>
2015-02-26 16:15:05 -0800

    add initial

---
+++ src/cs601/proxy/ClientHandler.java
@@ -0,0 +1,6 @@
+public class ClientHandler implements Runnable {
+	public static final int HTTP = 80;
+
+	protected boolean debug = true;
+
+}
---
+++ src/cs601/proxy/ProxyServer.java
@@ -0,0 +1,5 @@
+public class ProxyServer {
+	public static final boolean SingleThreaded = false;
+
+	public static void main(String[] args) { }
+}
